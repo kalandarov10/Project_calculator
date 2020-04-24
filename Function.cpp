d1d68211b734cf2a9d1d907c6e1852d869cbe868
kalandarov10 <64271161+kalandarov10@users.noreply.github.com>
2020-04-24 22:03:26 +0500

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include<iostream>
+#include <locale>
+#include<cmath>
+#define PI 3.141592653589793238492643
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	double x, F, a, b;
+
+	cout << "Вычисление значения функции\nАвтор : студент гр.РИ - 190008 Каландаров М.Ю. 2020 г." << endl; 
+
+	cout << " Введите x = ";
+	cin >> x;
+
+	if (x > 1)
+	{
+		b = pow(x, 2);
+		F = 0.5 * sin(PI * b);
+		cout << F;
+	}
+
+	else if (x > 0 && x <= 1)
+	{
+		a = 5.5 * x;
+		F = pow(a, 1. / 3) - x;
+		cout << F;
+	}
+
+	else
+	{
+		F = 0;
+		cout << F;
+	}
+}
